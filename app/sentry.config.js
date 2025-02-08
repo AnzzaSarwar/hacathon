abd8cee8a4d052f2f12519f680148d3078a2d77b
Anzabajwa <msarwarbajwamsarwarbajwa@gmail.com>
2025-02-07 16:16:53 -0800

    Updated Product page to display dynamic data

---
+++ app/sentry.config.js
@@ -0,0 +1,12 @@
+const { withSentryConfig } = require("@sentry/nextjs")
+
+
+const moduleExports ={
+
+};
+
+const SentryWebpackPluginOptions ={
+    silent:true 
+};
+
+module.exports = withSentryConfig (moduleExports,SentryWebpackPluginOptions);
